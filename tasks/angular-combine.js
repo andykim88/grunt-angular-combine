ba4bfde933637e1679f33a3ab57caea1e6be6539
andykim88 <andykim88@gmail.com>
2014-07-04 11:59:03 -0400

    Update angular-combine.js
    Changes:
    1) Combines partials and adds it to a single target file
    2) Uses grunt files implementation

--- tasks/angular-combine.js
+++ tasks/angular-combine.js
@@ -10,28 +10,29 @@
 
 module.exports = function (grunt) {
 	grunt.registerMultiTask('angularCombine', 'Combine AngularJS partials into a single HTML file.', function () {
-		var managePartialsDirectory = function(cwd, source, dest){
+		var managePartialsDirectory = function(cwd, source){
 			var destFileContent = "";
 			destFileContent += "<!-- Merge of " + source + " -->\n";
 
-			grunt.file.recurse(source, function (abspath, rootdir, subdir, filename) {
-				// only work with HTML files
-				if (/\.html$/.test(filename) && abspath.indexOf('/.') === -1) {
-					var id = abspath.substring(cwd.length + 1);
-					destFileContent += "<script type='text/ng-template' id='" + id + "'>\n";
-					destFileContent += grunt.file.read(abspath);
-					destFileContent += "</script>\n";
-				}
-			});
+			if (/\.html$/.test(source) && source.indexOf('/.') === -1) {
+				destFileContent += "<script type='text/ng-template' id='" + source.substring(cwd.length + 1) + "'>\n";
+				destFileContent += grunt.file.read(source);
+				destFileContent += "</script>\n";
+			}
 
-			grunt.file.write(dest + '.html', destFileContent);
+			return destFileContent;
 		};
+
+		var destContent = "";
 		this.files.forEach(function(file) {
 			var cwd = file.orig.cwd;
-			var dest = file.dest;
+
 			file.src.forEach(function(source) {
-				managePartialsDirectory(cwd, source, dest);
+				destContent += managePartialsDirectory(cwd, source);
 			});
 		});
+
+		var targetContent = grunt.file.read(this.data.options.target);
+		grunt.file.write(this.data.options.target, targetContent+destContent);
 	});
 };
